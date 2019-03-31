136eca71b8265e8856e442b73a292257f5a56a02
Travis CI User <travis@example.org>
2019-03-31 16:29:09 +0000

    Updates

--- api_project.js
+++ api_project.js
@@ -1,1 +1,17 @@
-define({  "template": {    "withCompare": false  },  "name": "@octokit/rest",  "version": "0.0.0-semantically-released",  "description": "GitHub REST API client for Node.js",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-03-31T15:58:56.124Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "template": {
+    "withCompare": false
+  },
+  "name": "@octokit/rest",
+  "version": "0.0.0-semantically-released",
+  "description": "GitHub REST API client for Node.js",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-31T16:29:06.120Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
